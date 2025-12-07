c3bbf20ab8005e0a984c57fa3ab8bb2931756e5f
Carlos <carlosccamino@ugr.es>
2025-12-07 22:05:59 +0100

    dev: asteroseismology - Adding sun constant and test.ipynb file

--- asteroseismology/constants.py
+++ asteroseismology/constants.py
@@ -7,5 +7,22 @@ Last Update: 11-11-2025
 Module to define common constants
 """
 
-class Constants:
+class Sun:
+    """
+    Class to provide solar constants:
+    
+    mass -> kg
+    radius -> m
+    logg -> dex
+    luminosity -> W
+    large separation -> c/d
+    Teff -> K
+    Z -> dex
+    """
+    mass = 1.9885*10**30 #kg
+    radius = 695700*10**3 #m
+    logg = 2.43775056 #dex
+    lum = 3.828*10**26 #W
+    teff = 5772 #K
+    delta_nu = 11.64672 #c/d
     
